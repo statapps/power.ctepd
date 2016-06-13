fc7080932871664d74f9370a18bbe5faafea01d2
Bingshu E. Chen <bingshu.chen@queensu.ca>
2016-06-13 10:27:41 -0400

    update

--- R/power.surv.test.R
+++ R/power.surv.test.R
@@ -1,14 +1,17 @@
 power.surv.test=function(n = NULL, sig.level = 0.05, s0 = NULL, s1 = NULL, 
-                         year = 3, followup = 4,
+                         year = 3, followup = 3,
                          power = NULL, tol = .Machine$double.eps^0.25){
   if (sum(sapply(list(n, s1, power), is.null)) != 1) 
-    stop("exactly one of 'n', 's1', 'power' must be NULL")
+    stop("exactly one of 'n', 's1', 'power', must be NULL")
 
   lambda0 = -log(s0)/year
-  p.body <- quote({
+
+  # initial output value for person.year and critical value k
+  person.year = 0
+  k = 0
+  p.body = quote({
     lambda1 = -log(s1)/year
     person.year = n*(1-exp(-lambda0*followup))/lambda0
-    person.year1 = n*(1-exp(-lambda1*followup))/lambda1
     
     k = qpois(sig.level, person.year*lambda0)
     alpha = ppois(k, person.year*lambda0)
@@ -17,9 +20,9 @@ power.surv.test=function(n = NULL, sig.level = 0.05, s0 = NULL, s1 = NULL,
       alpha =  ppois(k, person.year*lambda0)
     }
     sig.level = alpha
-      
-    pw = ppois(k, person.year1*lambda1)
-    ppois(k, n*lambda1*year)
+
+    # lambda*t = n*person.year1*lambda1  
+    ppois(k, n*(1-exp(-lambda1*followup)))
   })
 
   if(is.null(power))
@@ -28,7 +31,8 @@ power.surv.test=function(n = NULL, sig.level = 0.05, s0 = NULL, s1 = NULL,
     n = uniroot(function(n) eval(p.body) - power, c(25+1e-10, 1e+07), 
                 extendInt="upX", tol = tol)$root
   else if (is.null(s1))
-    s1 = uniroot(function(s1) eval(p.body) - power, c(s0, 0.98))$root
+    s1 = uniroot(function(s1) eval(p.body) - power, c(s0, 0.99))$root
+
   n = ceiling(n)
   power = eval(p.body)
   
